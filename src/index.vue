6d58f365fc54a94d5ba22ef00f07376254ee8e23
limin.zhang <limin.zhang@xwf-id.com>
2016-12-26 01:24:07 +0800

    2016.12.26 增加详细信息查看对话框。

--- src/index.vue
+++ src/index.vue
@@ -13,10 +13,41 @@
             </el-form-item>
         </el-form>
         <el-table :data="tableData" style="width: 100%">
-            <el-table-column prop="code" label="错误码" width="180"></el-table-column>
+            <el-table-column prop="code" label="错误码" width="90"></el-table-column>
             <el-table-column prop="title" label="错误信息" width="250"></el-table-column>
             <el-table-column prop="description" label="详细信息"></el-table-column>
+            <el-table-column prop="solution" label="解决办法"></el-table-column>
+            <el-table-column label="操作" inline-template :context="_self" fixed="right" width="150">
+              <el-dropdown trigger="click">
+                <el-button type="primary" size="small">
+                  操作菜单<i class="el-icon-caret-bottom el-icon--right"></i>
+                </el-button>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item><el-button type="primary" icon="information" @click="handleView(row)" >详细</el-button></el-dropdown-item>
+                  <el-dropdown-item><el-button type="primary" icon="edit">编辑</el-button></el-dropdown-item>
+                  <el-dropdown-item><el-button type="primary" icon="delete">删除</el-button></el-button></el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>           
+            </el-table-column>
         </el-table>
+
+        <!-- 对话框 //-->
+        <el-dialog title="详细信息" v-model="ui.dialogVisible">
+          <el-form :model="editForm" label-width="80px" ref="editForm">
+            <el-form-item label="错误码" prop="code">
+    					<el-input v-model="editForm.code" auto-complete="off" readonly="editForm.ui.dialogReadonly"></el-input>
+		    		</el-form-item>
+            <el-form-item label="错误信息" prop="title">
+              <el-input v-model="editForm.title" aria-autocomplete="off" readonly="editForm.ui.dialogReadonly"></el-input>
+            </el-form-item>
+            <el-form-item label="详细信息" prop="description">
+              <el-input type="textarea" v-model="editForm.description" aria-autocomplete="off" readonly="editForm.ui.dialogReadonly"></el-input>
+            </el-form-item>
+            <el-form-item label="解决办法" prop="solution">
+              <el-input type="textarea" v-model="editForm.solution" aria-autocomplete="off" readonly="editForm.ui.dialogReadonly"></el-input>
+            </el-form-item>
+          </el-form>
+        </el-dialog>
     <div>
 </template>
 <script>
@@ -26,6 +57,11 @@ export default {
   },
   data() {
     return {
+      ui: {
+        dialogVisible: false,
+        dialogReadonly: true
+      },
+      editForm: {},
       formInline: {
         user: '',
         region: ''
@@ -44,6 +80,14 @@ export default {
       }, (response) => { // Failure
 
       })
+    },
+    handleView: function(row) {
+      this.ui.dialogVisible = true;
+      this.editForm.id = row.id;
+      this.editForm.code = row.code;
+      this.editForm.title = row.title;
+      this.editForm.description = row.description;
+      this.editForm.solution = row.solution;
     },
     onSubmit: function() {
       console.log('submit!');
