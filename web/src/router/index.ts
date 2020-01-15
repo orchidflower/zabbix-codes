10b64d5eca26cda13034a510f1d015fc6878b017
limin.zhang <limin.zhang@eveus.com>
2020-01-15 08:05:28 +0800

    2020.01.15 删除不必要的文件。

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,26 +1,33 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Home from '@/views/Home.vue';
 import Index from '@/views/Index.vue';
 import Codes from '@/views/Codes.vue';
-import System from '@/views/Systems.vue';
-import Contact from '@/views/Contacts.vue';
+import Systems from '@/views/Systems.vue';
+import Contacts from '@/views/Contacts.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: '/',
-        component: Home, // App View
-        // components: {AppHeader: AppHeader, AppFooter: AppFooter},
-        children: [
-            { path: '/', component: Index },
-            { path: '/index', component: Index },
-            { path: '/codes/:code', component: Codes },
-            { path: '/systems', component: System },
-            { path: '/contacts', component: Contact }
-        ]
+        component: Index
+    },
+    {
+        path: '/index',
+        component: Index
+    },
+    {
+        path: '/systems',
+        component: Systems
+    },
+    {
+        path: '/contacts',
+        component: Contacts
+    },
+    {
+        path: '/codes/:code',
+        component: Codes
     }
 ];
 
